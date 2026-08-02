36f68d09afd35c9e7c1d85d58f02073ba8134179
marumn <marumn36@gmail.com>
2026-08-03 01:05:34 +0900

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,105 @@
+import {auth, db} from "./firebase.js";
+
+
+import {
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut
+}
+from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+
+
+window.registerUser = async function(){
+
+
+let email =
+document.getElementById("email").value;
+
+
+let password =
+document.getElementById("password").value;
+
+
+try{
+
+
+await createUserWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+
+alert("Account created!");
+
+
+location.href="../home";
+
+
+}
+catch(error){
+
+alert(error.message);
+
+}
+
+
+}
+
+
+
+
+window.loginUser = async function(){
+
+
+let email =
+document.getElementById("email").value;
+
+
+let password =
+document.getElementById("password").value;
+
+
+
+try{
+
+
+await signInWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+
+
+alert("Login successful!");
+
+
+location.href="../home";
+
+
+
+}
+catch(error){
+
+alert(error.message);
+
+}
+
+
+}
+
+
+
+
+window.logoutUser = async function(){
+
+
+await signOut(auth);
+
+
+location.reload();
+
+
+}
