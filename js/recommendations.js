c1a63e2778a744e86f28e85a80f9dee6e2a271c5
marumn <marumn36@gmail.com>
2026-08-21 21:51:56 +0900

    Create recommendations.js

---
+++ js/recommendations.js
@@ -0,0 +1,27 @@
+async function loadRecommendations() {
+
+    const response = await fetch("../data/manhwas.json");
+    const manhwas = await response.json();
+
+    const otherManhwas = manhwas.filter(
+        manga => manga.id !== window.mangaId
+    );
+
+    otherManhwas.sort(() => Math.random() - 0.5);
+
+    const recommendations = otherManhwas.slice(0, 3);
+
+    const grid = document.getElementById("recommendedGrid");
+
+    grid.innerHTML = recommendations.map(manga => `
+        <div class="card" onclick="location.href='${manga.link}'">
+
+            <img src="${manga.cover}" alt="${manga.title}">
+
+            <h3>${manga.title}</h3>
+
+        </div>
+    `).join("");
+}
+
+loadRecommendations();
